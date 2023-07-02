c34f6c39749bfb0c4620b349c5f7948dafb5ff80
Marianna <Marianna.l.americo@gmail.com>
2023-07-02 20:14:11 -0300

    Exemplo Java Records

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa("João", 25);
+        Pessoa pessoa2 = new Pessoa("Maria", 30);
+
+        System.out.println(pessoa1.nome());
+        System.out.println(pessoa2);
+    }
+}
---
+++ Pessoa.java
@@ -0,0 +1,1 @@
+public record Pessoa(String nome, int idade) {}
