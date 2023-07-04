1fe250ff811e9353dd7d18aae554b95dd0c447bf
ndbzika <chakcpps@gmail.com>
2023-07-03 23:49:36 -0300

    Comentando código para explicar

--- HospedeMain.java
+++ HospedeMain.java
@@ -1,11 +1,11 @@
-import java.util.List;
+// import java.util.List;
 
-public class HospedeMain {
-  public static void main(String[] args) {
-    List<HospedeResponseDTO> hospedesList = hospedeService.findAll().stream().map(HospedeResponseDTO::new).toList();
+// public class HospedeMain {
+//   public static void main(String[] args) {
+//     List<HospedeResponseDTO> hospedesList = hospedeService.findAll().stream().map(HospedeResponseDTO::new).toList();
     
-    for (HospedeResponseDTO hospede: hospedesList) {
-      System.out.println(hospede.id() + " " + hospede.nome() + " " + hospede.email() + " " + hospede.telefone());
-    }
-  }
-}
+//     for (HospedeResponseDTO hospede: hospedesList) {
+//       System.out.println(hospede.id() + " " + hospede.nome() + " " + hospede.email() + " " + hospede.telefone());
+//     }
+//   }
+// }
--- HospedeResponseDTO.java
+++ HospedeResponseDTO.java
@@ -1,5 +1,5 @@
-public record HospedeResponseDTO(Integer id, String nome, String email, String telefone) {
-    public HospedeResponseDTO(Hospede hospede) {
-        this(hospede.getId(), hospede.getNome(), hospede.getEmail(), hospede.getTelefone());
-    }
-}
+// public record HospedeResponseDTO(Integer id, String nome, String email, String telefone) {
+//     public HospedeResponseDTO(Hospede hospede) {
+//         this(hospede.getId(), hospede.getNome(), hospede.getEmail(), hospede.getTelefone());
+//     }
+// }
