cbb83a722a8225666160e87d05a09775e6fe2fd7
AntonioMarcosDev <ambrcz@hotmail.com>
2023-07-03 22:43:29 -0300

    Adicionando Exemplos VarArgs

---
+++ VarArgsNativos.java
@@ -0,0 +1,10 @@
+public class VarArgsNativos {
+
+    /*public PrintStream printf(String format, Object... args) {
+        return this.format(format, args);
+    }*/
+
+    public static void main(String[] args) {
+        System.out.printf("%d %d", 1, 2);
+    }
+}
